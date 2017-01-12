71ee0df88a37026e3ee0b3c34f95eeaddeebba8a
Gurdit Singh Bedi <bedi.gsb.96@gmail.com>
2017-01-12 21:38:21 +0530

    Changed The Design

--- js/main.js
+++ js/main.js
@@ -1,1 +1,8 @@
 
+const button = document.getElementsByTagName('button')[0];
+
+button.addEventListener('click', function(e) {
+    const h = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
+    window.scroll(0, h);
+
+});
