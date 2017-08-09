3c66b690d7b1ba3c103d4b8a16c1ef29484d5e96
Gurdit Singh Bedi <gurdisbedi@gmail.com>
2017-08-09 18:11:28 +0530

    Social Networking icons remain sticked to each other.

--- js/main.js
+++
@@ -1,8 +0,0 @@
-
-const button = document.getElementsByTagName('button')[0];
-
-button.addEventListener('click', function(e) {
-    const h = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
-    window.scroll(0, h);
-
-});
